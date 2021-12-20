6f224fdeaf03d328d3d5619a03e2b9a6fed8bf79
tugrulguner <tugrulgunr@gmail.com>
2021-12-20 12:04:37 -0500

    ingestion.txt updated

--- ingestion.py
+++ ingestion.py
@@ -18,19 +18,31 @@ output_folder_path = config['output_folder_path']
 
 #############Function for data ingestion
 def merge_multiple_dataframe():
+    ingestedlist = []
     df = pd.DataFrame(columns=['corporation','lastmonth_activity','lastyear_activity','number_of_employees','exited'])
     for file in os.listdir(os.getcwd()+'/'+input_folder_path):
         try:
             read_data = pd.read_csv(os.getcwd()+'/'+input_folder_path+'/'+file)
             df = df.append(read_data).reset_index(drop=True)
+            ingestedlist.append(file)
         except pd.errors.EmptyDataError:
             continue
     
+    # Drop duplicated values
+    df.drop_duplicates(inplace=True, ignore_index=True)
+
+    # Create output folder if not exist and save files
     if os.path.isdir(os.getcwd()+'/'+output_folder_path):
         df.to_csv(os.getcwd()+'/'+output_folder_path+'/finaldata.csv')
+        with open(os.getcwd()+'/'+output_folder_path+'/ingestedfiles.txt', 'w') as f:
+            for el in ingestedlist:
+                f.write(el+'\n')
     else:
         os.mkdir(os.getcwd()+'/'+output_folder_path)
         df.to_csv(os.getcwd()+'/'+output_folder_path+'/finaldata.csv')
+        with open(os.getcwd()+'/'+output_folder_path+'/ingestedfiles.txt', 'w') as f:
+            for el in ingestedlist:
+                f.write(el+'\n')
 
 if __name__ == '__main__':
     merge_multiple_dataframe()
